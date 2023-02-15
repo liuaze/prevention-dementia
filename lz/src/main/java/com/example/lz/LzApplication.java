910f16758889904aee549767889f4b1b08ab5092
liuaze <1760796876@qq.com>
2023-02-15 13:33:35 +0800

    初始化

---
+++ lz/src/main/java/com/example/lz/LzApplication.java
@@ -0,0 +1,13 @@
+package com.example.lz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzApplication.class, args);
+    }
+
+}
---
+++ lz/src/test/java/com/example/lz/LzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
